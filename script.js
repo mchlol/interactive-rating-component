8f5e5f35baaa283f66247be7790a25f7026a823c
mchlol <m.andreabr@gmail.com>
2022-06-12 11:40:44 +1000

    start algorithm

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('it works');
+
+/* 
+select all the ratings
+add a click event listener to all the ratings
+the callback function will:
+- assign the selected rating to a variable
+- toggle the active display setting
+- show the selected rating in a span
+*/
